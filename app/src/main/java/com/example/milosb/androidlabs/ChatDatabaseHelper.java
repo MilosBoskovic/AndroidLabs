90549193e5282580e567e0fa31daffd374462b6b
bosk0010 <milos.boskovic.1988@gmail.com>
2018-02-21 15:46:04 -0500

    lab 5 task 4 done

---
+++ app/src/main/java/com/example/milosb/androidlabs/ChatDatabaseHelper.java
@@ -0,0 +1,42 @@
+package com.example.milosb.androidlabs;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Milos B on 2018-02-21.
+ */
+
+public class ChatDatabaseHelper extends SQLiteOpenHelper {
+
+    private static String DATABASE_NAME = "Messages.db";
+    private static int VERSION_NUM = 1;
+    final static String KEY_ID = "Id";
+    final static String KEY_MESSAGE = "Message";
+    final static String TABLE_NAME = "ChatHistory";
+
+    public ChatDatabaseHelper(Context ctx) {
+        super(ctx, DATABASE_NAME, null, VERSION_NUM);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+
+        db.execSQL("create table " + TABLE_NAME + "( " + KEY_ID + " integer primary key autoincrement, " + KEY_MESSAGE + " text);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){  // newVersion > oldVersion
+
+        db.execSQL("drop table if exists " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {  // newVersion < oldVersion
+
+        db.execSQL("drop table if exists " + TABLE_NAME);
+        onCreate(db);
+    }
+}
